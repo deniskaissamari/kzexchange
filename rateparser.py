dd96597f69fe7f809dde542a15fae590054b0bf3
Klim Kozlov <klim.kozlov.05@gmail.com>
2022-11-02 13:55:16 +0400

    aboba6

--- rateparser.py
+++ rateparser.py
@@ -4,6 +4,7 @@ from fake_useragent import UserAgent
 
 
 def get_parse_rate_main():
+    kzrate = 0
     headers = {
         'User-Agent': UserAgent().chrome
     }
@@ -12,7 +13,14 @@ def get_parse_rate_main():
     soup = BeautifulSoup(r.text, 'lxml')
     rates_names = soup.find_all("p", style="text-align: left;")   # ищем названия всех валют
     rates = soup.find_all("p", style="text-align: center;")       # ищем курс всех валют
-    rates_name = rates_names[6].text.strip()                      # создаем переменную с названием казахстанского тенге
-    raterubkz = 1/float(rates[6].text.strip().replace(",", "."))  # создаем переменную с курсом реблей в тенге
-    ratekzrub = float(rates[6].text.strip().replace(",", "."))    # создаем переменную с курсом тенге в рубли
+    for i in range(len(rates_names)):
+        a = rates_names[i].text.strip()
+        if a.count("Казахстанский тенге") > 0:
+            kzrate = i
+    rates_name = rates_names[kzrate].text.strip()                      # создаем переменную с названием казахстанского тенге
+    raterubkz = 1/float(rates[kzrate].text.strip().replace(",", "."))  # создаем переменную с курсом реблей в тенге
+    ratekzrub = float(rates[kzrate].text.strip().replace(",", "."))    # создаем переменную с курсом тенге в рубли
     return [rates_name, raterubkz, ratekzrub]
+
+
+print(get_parse_rate_main())
