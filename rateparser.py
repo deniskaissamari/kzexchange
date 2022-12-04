de19ba264fa46fb94bf8d71f8312a2d0a0480518
deniskaissamari <113232187+deniskaissamari@users.noreply.github.com>
2022-12-04 15:10:46 +0400

    Update rateparser.py

--- rateparser.py
+++ rateparser.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
+# from fake_useragent import UserAgent
 
 
 def get_parse_rate_main():
     kzrate = 0
     headers = {
-        'User-Agent': UserAgent().chrome
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62'
     }
     url = "https://mironline.ru/support/list/kursy_mir/"
     r = requests.get(url, headers=headers)                        # достаём код страницы
